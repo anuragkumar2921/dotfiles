44a9905f52a49b9911ce6fd75e471d0c136756bf
anuragkumar <anuragkumar5144@gmail.com>
2024-09-13 02:14:59 +0530

    karabinar configs 2

---
+++ .config/karabiner/rules.ts
@@ -0,0 +1,45 @@
+import fs from "fs";
+import { KarabinerRules } from "./types";
+import { createHyperSubLayers, app, open, rectangle, shell } from "./utils";
+
+const rules: KarabinerRules[] = [
+  // Define the Hyper key itself
+  {
+    description: "Hyper Key (⌃⌥⇧⌘)",
+    manipulators: [
+      {
+        description: "Caps Lock -> Hyper Key",
+        from: {
+          key_code: "caps_lock",
+        },
+        to_if_alone: [
+          {
+            key_code: "escape",
+          },
+        ],
+        type: "basic",
+      },
+    ],
+  },
+];
+
+fs.writeFileSync(
+  "karabiner.json",
+  JSON.stringify(
+    {
+      global: {
+        show_in_menu_bar: false,
+      },
+      profiles: [
+        {
+          name: "Default",
+          complex_modifications: {
+            rules,
+          },
+        },
+      ],
+    },
+    null,
+    2
+  )
+);
