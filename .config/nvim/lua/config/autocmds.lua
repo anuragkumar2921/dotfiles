8cf7fa9e60b4baa30ceb8645985f4f49f1b838a2
anuragkumar <anuragkumar5144@gmail.com>
2024-09-15 21:38:45 +0530

    changelog - sketchybar, import organize, update yabrc to support sketchybar

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,18 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Auto-organize imports on save for multiple languages
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = vim.api.nvim_create_augroup("auto_imports", { clear = true }),
+  pattern = { "*.java" },
+  callback = function()
+    vim.lsp.buf.code_action({
+      apply = true,
+      context = {
+        only = { "source.organizeImports" },
+        diagnostics = {},
+      },
+    })
+  end,
+})
