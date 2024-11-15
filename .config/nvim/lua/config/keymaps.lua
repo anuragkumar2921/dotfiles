9e8fadc66af55c50c5e8c81d48c83081e25cc2ed
anuragkumar <anuragkumar5144@gmail.com>
2024-11-15 14:39:35 +0530

    add keymaps, cmp, telescope custom keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,6 +8,15 @@ local keymap = vim.keymap -- for conciseness
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- navigation zz shortcuts
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+
+-- paste without storing it into register
+keymap.set("x", "<leader>p", [["_dP]])
+
 -- Leetcode keymaps
 keymap.set("", "<leader>'", ":Leet run<CR>", { noremap = true, silent = true })
 keymap.set("", "<leader><CR>", ":Leet submit<CR>", { noremap = true, silent = true })
