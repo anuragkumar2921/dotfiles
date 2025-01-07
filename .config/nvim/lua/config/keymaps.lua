781837ee8adcfc0c5c1f7d2b5840157de9ae144a
anuragkumar <anuragkumar5144@gmail.com>
2025-01-07 12:34:18 +0530

    keymap update

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -41,5 +41,5 @@ keymap.set("n", "<C-p>", "o<Esc>p", opts)
 -- tabs
 keymap.set("n", "<leader><tab>n", "<cmd>tabnew<cr>", { desc = "New Tab" })
 keymap.set("n", "<leader><tab><tab>", "g<tab>", { desc = "go to Last visited Tab" })
--- keymap.set("n", "<tab>l", "<cmd>tabnext<cr>", { desc = "go to Next Tab" })
--- keymap.set("n", "<tab>h", "<cmd>tabprevious<cr>", { desc = "go to Previous Tab" })
+keymap.set("n", "<leader><tab>l", "<cmd>tabnext<cr>", { desc = "go to Next Tab" })
+keymap.set("n", "<leader><tab>h", "<cmd>tabprevious<cr>", { desc = "go to Previous Tab" })
